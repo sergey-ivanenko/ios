a34d950dd232de06132ee80e3067b734011d57fb
Sergey Ivanenko <storm06rk@gmail.com>
2019-04-02 00:41:10 +0300

    Solved first and second tasks.

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,24 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+    [magaine retain];
+    [note retain];
+    //[magaine autorelease];
+    //[note autorelease];
+    NSMutableString* str = [NSMutableString stringWithCapacity:magaine.length];
+    [str appendString:magaine.lowercaseString];
+    
+    NSArray* noteArray = [note.lowercaseString componentsSeparatedByString:@" "];
+    //[noteArray autorelease];
+    
+    for (NSString* word in noteArray) {
+        if (![str containsString:word.lowercaseString]) {
+            return NO;
+        }
+        [str setString:[str stringByReplacingOccurrencesOfString:word withString:@""]];
+    }
+    
+    return YES;
+}
 @end
--- Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
@@ -1,5 +1,74 @@
 #import "RomanTranslator.h"
 
 @implementation RomanTranslator
-// your code here
+
+- (NSString *)romanFromArabic:(NSString *)arabicString {
+    [arabicString retain];
+    
+    NSDictionary* arabicRoman = [NSDictionary dictionaryWithObjectsAndKeys:
+                                               @"M", [NSNumber numberWithInteger:1000],
+                                               @"CM", [NSNumber numberWithInteger:900],
+                                               @"D", [NSNumber numberWithInteger:500],
+                                               @"CD", [NSNumber numberWithInteger:400],
+                                               @"C", [NSNumber numberWithInteger:100],
+                                               @"XC", [NSNumber numberWithInteger:90],
+                                               @"L", [NSNumber numberWithInteger:50],
+                                               @"XL", [NSNumber numberWithInteger:40],
+                                               @"X", [NSNumber numberWithInteger:10],
+                                               @"IX", [NSNumber numberWithInteger:9],
+                                               @"V", [NSNumber numberWithInteger:5],
+                                               @"IV", [NSNumber numberWithInteger:4],
+                                               @"I", [NSNumber numberWithInteger:1], nil];
+    
+    NSMutableString *romanString = [NSMutableString string];
+    NSInteger arabic = arabicString.integerValue;
+    NSArray* keys = [[[[arabicRoman allKeys]
+                       sortedArrayUsingSelector:@selector(compare:)]
+                       reverseObjectEnumerator] allObjects];
+    
+    for (int i = 0; i < arabicRoman.count; i++) {
+        if (arabic > 0) {
+            for (NSNumber* key in keys) {
+                if (arabic >= key.integerValue) {
+                    arabic -= key.integerValue;
+                    [romanString appendString:[arabicRoman objectForKey:key]];
+                }
+            }
+        }
+    }
+    
+    //[arabicString release];
+    //[romanString autorelease];
+    return romanString;
+}
+
+- (NSString *)arabicFromRoman:(NSString *)romanString {
+    [romanString retain];
+    
+    NSInteger result = [self toArabic:romanString];
+    
+    return [NSString stringWithFormat:@"%ld", result];
+}
+
+-(NSInteger)toArabic:(NSString*)romanString {
+    [romanString retain];
+    
+    NSArray* arabicValues = @[@(1000), @(900), @(500), @(400),
+                             @(100), @(90), @(50), @(40),
+                             @(10), @(9), @(5), @(4), @(1)];
+    NSArray* romanValues = @[@"M", @"CM", @"D", @"CD",
+                            @"C", @"XC", @"L", @"XL",
+                            @"X", @"IX", @"V", @"IV", @"I"];
+    
+    NSMutableString* roman = [NSMutableString stringWithFormat:@"%@", romanString];
+    
+    for (int i = 0; i < romanValues.count; i++) {
+        if([roman hasPrefix:[romanValues objectAtIndex:i]]) {
+            return [[arabicValues objectAtIndex:i] integerValue] + [self toArabic:[roman stringByReplacingCharactersInRange:NSMakeRange(0, [[romanValues objectAtIndex:i] length]) withString:@""]];
+        }
+    }
+    
+    return 0;
+}
+
 @end
